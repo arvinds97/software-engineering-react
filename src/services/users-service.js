b08098dcf1eb1452ab966798e6c71fb1bdec6fd8
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-03 07:59:11 -0400

    Corrected URL end-point

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://cs5500-node-project-arvinds97.herokuapp.com/";
+const BASE_URL = "https://cs5500-node-project-arvinds97.herokuapp.com";
 // const BASE_URL = "https://software-engineering-node-fa22.herokuapp.com/api";
 // const BASE_URL = "http://localhost:4000/api";
 
