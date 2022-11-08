9c847eeeb51329da6c0d86f099bedc749910fc81
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-07 19:19:46 -0500

    a3 fse fall 2022

--- src/tests/tuit-list.test.js
+++ src/tests/tuit-list.test.js
@@ -7,7 +7,7 @@ import {createTuit} from "../services/tuits-service";
 
 
 const MOCKED_USERS = [
-    {username: 'alice', password: 'lv426', email: 'repley@weyland.com', _id: "123"},
+    {username: 'alice', password: 'lv426', email: 'repley@weyland.com'},
     {username: 'bob', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"},
     {username: 'charlie', password: 'illbeback', email: 'sarah@bigjeff.com', _id: "234"}
 ]
@@ -17,19 +17,16 @@ const MOCKED_TUITS = [
         tuit: "alice's tuit",
         postedBy: MOCKED_USERS[0],
         postedOn: "2022-11-15T00:00:00.000Z",
-        _id: "123"
     },
     {
         tuit: "bob's tuit",
         postedBy: MOCKED_USERS[1],
         postedOn: "2022-11-15T00:00:00.000Z",
-        _id: "234"
     }
     ,{
         tuit: "charlie's tuit",
         postedBy: MOCKED_USERS[2],
         postedOn: "2022-11-15T00:00:00.000Z",
-        _id:"456"
     }
 ];
 
@@ -47,17 +44,21 @@ test('tuit list renders static tuit array', () => {
 
 test('tuit list renders async', async () => {
     const ripley = {
-        username: 'ellenripley',
-        password: 'lv426',
-        email: 'ellenripley@aliens.com'
+        username: 'alice', password: 'lv426', email: 'repley@weyland.com'
     }
     let newUser;
     newUser = await createUser(ripley);
-    const newTuit = await createTuit(newUser._id, MOCKED_TUITS[0]);
-
-    expect(newTuit.tuit).toEqual(MOCKED_TUITS[0].tuit);
-    expect(newTuit.postedOn).toEqual(MOCKED_TUITS[0].postedOn);
-    expect(newTuit.postedBy).toEqual(MOCKED_TUITS[0].postedBy);
+    const newTuit1 = await createTuit(newUser._id, MOCKED_TUITS[0]);
+    const newTuit2 = await createTuit(newUser._id, MOCKED_TUITS[1]);
+    const newTuit3 = await createTuit(newUser._id, MOCKED_TUITS[2]);
+    render(
+        <HashRouter>
+            <Tuit tuit={newTuit1}/>
+            <Tuit tuit={newTuit2}/>
+            <Tuit tuit={newTuit3}/>
+        </HashRouter>);
+    const linkElement = screen.getByText(/alice's tuit/i);
+    expect(linkElement).toBeInTheDocument();
 })
 
 
